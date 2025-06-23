32e70e5737911a1b70503561032661ce26ed00e3
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-23 17:04:41 +0530

    added policy

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -4,8 +4,22 @@ resource "aws_s3_bucket" "cloudtrail_logs" {
   versioning {
     enabled = true
   }
-
+  # Attach the S3 bucket policy allowing CloudTrail to write logs
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "s3:PutObject"
+        Resource = "${aws_s3_bucket.cloudtrail_logs.arn}/*"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
+
 
 resource "random_pet" "this" {
   length = 2
