5ff5f69556dd2c5ea194905c9b3b241771029467
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-23 15:27:30 +0530

    added few changes

--- main.tf
+++ main.tf
@@ -1,27 +1,26 @@
-provider "aws" {
-  region = var.region
-}
-
 module "S3" {
   source           = "./module/s3"
   s3_bucket_name   = var.cloudtrail_s3_bucket_name
   
-}
-module "cloudtrail" {
-  source                = "./modules/cloudtrail"
-  cloudwatch_log_group  = var.cloudwatch_log_group
-  cloudtrail_log_role_arn = module.cloudwatch.log_role_arn
-}
-
-module "cloudwatch" {
-  source = "./modules/cloudwatch"
-  log_group_name = var.cloudwatch_log_group
 }
 
 module "sns" {
   source           = "./modules/sns"
   email_recipient  = var.email_recipient
   sns_topic_name   = var.sns_topic_name
+}
+
+module "cloudtrail" {
+  source                = "./modules/cloudtrail"
+  s3_bucket_name        = module.S3.cloudtrail_s3_bucket_arn
+  cloudwatch_log_group  = var.cloudwatch_log_group
+  cloudtrail_log_role_arn = module.cloudwatch.log_role_arn
+}
+
+module "cloudwatch" {
+  source = "./modules/cloudwatch"
+  sns_topic_arn = module.sns.sns_topic_arn
+  log_group_name = var.cloudwatch_log_group
 }
 
 output "sns_topic_arn" {
